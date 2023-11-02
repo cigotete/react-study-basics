ffd5a1dcf61cd15d5b9cecde41ea6e5d70013665
Camilo Figueroa <cigotete@gmail.com>
2023-11-02 17:32:33 -0500

    Displaying fetched data.

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -5,11 +5,30 @@ import { useFetch } from '../hooks/useFetch';
 export const MultipleCustomHooks = () => {
 
   const { data, loading, error } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes`);
-  console.log(data, loading, error);
+  const { author, quote } = !!data && data[0];
+  // !!data = data recives undefined, but ! gives false and !! gives true
 
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
+      <hr />
+
+      {
+        loading
+        ?
+        (
+          <div className="alert alert-info text-center">
+            Loading...
+          </div>
+        )
+        :
+        (
+          <blockquote className="blockquote text-end">
+            <p className="mb-3">{ quote }</p>
+            <footer className="blockquote-footer">{ author }</footer>
+          </blockquote>
+        )
+      }
     </>
   )
 }
