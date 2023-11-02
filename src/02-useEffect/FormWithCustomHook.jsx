9daaa3dec55c51cdc7b628b956062e02a0ef8f31
Camilo Figueroa <cigotete@gmail.com>
2023-11-02 12:32:49 -0500

    Custom hook to handle forms.

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,45 @@
+import { useForm } from '../hooks/useForm';
+
+export const FormWithCustomHook = () => {
+
+  const { formState, handleInputChange } = useForm({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const { username, email, password } = formState;
+
+  return (
+    <>
+      <h1>FormState test</h1>
+      <hr />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="username"
+        name="username"
+        value={ username }
+        onChange={ handleInputChange }
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="username@here.com"
+        name="email"
+        value={ email }
+        onChange={ handleInputChange }
+      />
+
+      <input
+        type="password"
+        className="form-control mt-2"
+        placeholder="password"
+        name="password"
+        value={ password }
+        onChange={ handleInputChange }
+      />
+    </>
+  )
+}
