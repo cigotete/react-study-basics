cdaeb3089b04d0d9df7ef8521a5ed383f65bf59b
Camilo Figueroa <cigotete@gmail.com>
2023-11-03 11:17:03 -0500

    Understanding memo to avoid re-rendering a component.

---
+++ src/06-memos/Small.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { memo } from 'react';
+import PropTypes from 'prop-types';
+
+export const Small = React.memo(({ value }) => {
+    console.log(' Me volví a dibujar :( ');
+    return (
+        <small>{ value }</small>
+    )
+});
+
+Small.displayName = 'Small';
+Small.propTypes = {
+    value: PropTypes.number.isRequired,
+  };
+  
