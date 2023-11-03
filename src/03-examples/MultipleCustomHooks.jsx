07d720e0cebdd72406e31c7cb85185ad22446ead
Camilo Figueroa <cigotete@gmail.com>
2023-11-03 01:25:25 -0500

    Implementing Quotes and LoadingQuotes components.

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,4 +1,5 @@
 import { useFetch, useCounter } from '../hooks';
+import { LoadingQuote, Quote } from './';
 
 export const MultipleCustomHooks = () => {
   const { counter, increment } = useCounter(1);
@@ -13,19 +14,8 @@ export const MultipleCustomHooks = () => {
 
       {
         loading
-        ?
-        (
-          <div className="alert alert-info text-center">
-            Loading...
-          </div>
-        )
-        :
-        (
-          <blockquote className="blockquote text-end">
-            <p className="mb-3">{ quote }</p>
-            <footer className="blockquote-footer">{ author }</footer>
-          </blockquote>
-        )
+        ? <LoadingQuote text="Loading..." />
+        : <Quote quote={ quote } author={ author } />
       }
 
       <button
