821f193e272c4654ae438eecb1979075e65263d9
Camilo Figueroa <cigotete@gmail.com>
2023-11-06 15:26:46 -0500

    Uncaught SyntaxError: 'undefined' is not valid JSON at JSON.parse (<anonymous>)

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,6 +1,9 @@
 
 export const todoReducer = (initialState = [], action) => {
   switch (action.type) {
+    case '[TODO] add':
+      return [...initialState, action.payload];
+
     default:
       return initialState;
   }
