7faee5d9e90dbd11c82084afc00dad7c83455c6b
Camilo Figueroa <cigotete@gmail.com>
2023-11-07 11:56:05 -0500

    Implemented components to review routing basics.

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar, AboutPage, HomePage, LoginPage } from './';
+
+export const MainApp = () => {
+  return (
+    <>
+    <h1>MainApp</h1>
+    <Navbar />
+    <hr />
+
+    <Routes>
+      <Route path="/" element={ <HomePage /> } />
+      <Route path="/about" element={ <AboutPage /> } />
+      <Route path="/login" element={ <LoginPage /> } />
+      <Route path="*" element={ <Navigate to="/login" /> } />
+    </Routes>
+    </>
+  )
+}
---
+++ src/09-useContext/index.js
@@ -0,0 +1,4 @@
+export * from './Navbar';
+export * from './AboutPage';
+export * from './HomePage';
+export * from './LoginPage';
